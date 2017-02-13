56de98f64946d1b7cfde5d607e19642a4c0cce55
ronny <ronnymacmaster@gmail.com>
2017-02-12 20:31:59 -0600

    tabs to spaces

--- systems/hw/src/hw2/cyclicbarrier/CyclicBarrier.java
+++ systems/hw/src/hw2/cyclicbarrier/CyclicBarrier.java
@@ -8,61 +8,63 @@ import java.util.concurrent.Semaphore;
  * the given number of parties (threads) are waiting upon it
  */
 public class CyclicBarrier {
-	
-	private final int parties;
-	private int position;
-	private Semaphore mutex;
-	private Semaphore barrier;
-	private Semaphore waiter;
-	
-	/** CyclicBarrier <br>
-	 * 
-	 * Constructs a new CyclicBarrier Object. <br>
-	 */
-	public CyclicBarrier(int parties) {
-		// Creates a new CyclicBarrier that will trip when
-		// the given number of parties (threads) are waiting upon it
-		
-		if (parties <= 0) { throw new IllegalArgumentException("parties must be > 0"); }
-		
-		this.parties = parties;
-		this.position = 1;
-		mutex = new Semaphore(1);
-		barrier = new Semaphore(parties);
-		waiter = new Semaphore(0);
-	}
-	
-	int await() throws InterruptedException {
-		// Waits until all parties have invoked await on this barrier.
-		// If the current thread is not the last to arrive then it is
-		// disabled for thread scheduling purposes and lies dormant until
-		// the last thread arrives.
-		// Returns: the arrival index of the current thread, where index
-		// (parties - 1) indicates the first to arrive and zero indicates
-		// the last to arrive.
-		
-		barrier.acquire();
-		
-		// get barrier rank
-		mutex.acquire();
-		int rank = parties - position;
-		position = position + 1;
-		mutex.release();
-		
-		// wait for others
-		if (rank > 0) {
-			waiter.acquire();
-			return rank;
-		}
-		
-		// continue and reset
-		else {
-			position = 1;
-			waiter.release(parties - 1);
-			barrier.release(parties);
-			return rank;
-		}
-		
-	}
-	
+
+    private final int parties;
+    private int position;
+    private Semaphore mutex;
+    private Semaphore barrier;
+    private Semaphore waiter;
+
+    /** CyclicBarrier <br>
+     * 
+     * Constructs a new CyclicBarrier Object. <br>
+     */
+    public CyclicBarrier(int parties) {
+        // Creates a new CyclicBarrier that will trip when
+        // the given number of parties (threads) are waiting upon it
+
+        if (parties <= 0) {
+            throw new IllegalArgumentException("parties must be > 0");
+        }
+
+        this.parties = parties;
+        this.position = 1;
+        mutex = new Semaphore(1);
+        barrier = new Semaphore(parties);
+        waiter = new Semaphore(0);
+    }
+
+    int await() throws InterruptedException {
+        // Waits until all parties have invoked await on this barrier.
+        // If the current thread is not the last to arrive then it is
+        // disabled for thread scheduling purposes and lies dormant until
+        // the last thread arrives.
+        // Returns: the arrival index of the current thread, where index
+        // (parties - 1) indicates the first to arrive and zero indicates
+        // the last to arrive.
+
+        barrier.acquire();
+
+        // get barrier rank
+        mutex.acquire();
+        int rank = parties - position;
+        position = position + 1;
+        mutex.release();
+
+        // wait for others
+        if (rank > 0) {
+            waiter.acquire();
+            return rank;
+        }
+
+        // continue and reset
+        else {
+            position = 1;
+            waiter.release(parties - 1);
+            barrier.release(parties);
+            return rank;
+        }
+
+    }
+
 }
--- systems/hw/src/hw2/cyclicbarrier/MonitorBarrierThread.java
+++ systems/hw/src/hw2/cyclicbarrier/MonitorBarrierThread.java
@@ -9,36 +9,36 @@ import java.util.Set;
  * TODO: Document <br>
  */
 public class MonitorBarrierThread extends Thread {
-	
-	private int id;
-	private MonitorCyclicBarrier barrier;
-	private static Set<Integer> threads = new HashSet<Integer>(); 
-	
-	/** BarrierThread <br>
-	 * 
-	 * Constructs a new BarrierThread Object. <br>
-	 */
-	public MonitorBarrierThread(int id, MonitorCyclicBarrier barrier) {
-		this.id = id;
-		this.barrier = barrier;
-		threads.add(id);
-	}
-	
-	@Override
-	public void run() {
-		try {
-			System.out.format("Thread %d: Now entering barrier%n", id);
-			int position = barrier.await();
-			threads.remove(id);
-			System.out.format("Thread %d: Woohoo (position %d), I'm Free!!!%n", id, position);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public static void PrintThreadSet(){
-		System.out.println(threads);
-	}
-	
+
+    private int id;
+    private MonitorCyclicBarrier barrier;
+    private static Set<Integer> threads = new HashSet<Integer>();
+
+    /** BarrierThread <br>
+     * 
+     * Constructs a new BarrierThread Object. <br>
+     */
+    public MonitorBarrierThread(int id, MonitorCyclicBarrier barrier) {
+        this.id = id;
+        this.barrier = barrier;
+        threads.add(id);
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.format("Thread %d: Now entering barrier%n", id);
+            int position = barrier.await();
+            threads.remove(id);
+            System.out.format("Thread %d: Woohoo (position %d), I'm Free!!!%n", id, position);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void PrintThreadSet() {
+        System.out.println(threads);
+    }
+
 }
